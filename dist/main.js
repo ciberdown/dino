47a5f7a0c4f8c46f30854d5eff2c019ed2918dd4
ciberdown <teymuri.amin@gmail.com>
2023-04-23 14:56:40 +0330

    clouds and ground moving added

--- dist/main.js
+++ dist/main.js
@@ -1,26 +1,54 @@
 "use strict";
 class Dino {
     constructor() {
-        this._jump = 1;
-        this._speed = 1;
-        this._jump = this._jump;
-        this._speed = this._speed;
+        this._jump_mt = 1;
+        this._speed_num = 1;
+        this.header_text_one = "T-Rex Chrome Dino Game";
+        this.text_one = "T-Rex Dinosaur - a replica of the hidden game from Chrome offline mode. Press Space to start the game online and jump your Dino, use down arrow (↓) to duck.";
+        this._t_rex = document.getElementById("t_rex");
+        this._jump_mt = this._jump_mt;
+        this._speed_num = this._speed_num;
+        this.game_logic_control();
     }
     get speed() {
-        return this._speed;
+        return this._speed_num;
     }
     set speed(num) {
         if (num > 0)
-            this._speed = num;
+            this._speed_num = num;
     }
-    get get_jump() {
-        return this._jump;
+    get jump_mt() {
+        return this._jump_mt;
     }
-    set set_jump(num) {
+    set jump_mt(num) {
         if (num > 0)
-            this._jump = num;
+            this._jump_mt = num;
+    }
+    game_logic_control() {
+        this.ground_dots_moving();
+    }
+    jump() {
+    }
+    trees_moving() {
+    }
+    clouds_moving() {
+    }
+    ground_dots_moving() {
+        for (let i = 1; i < 500; i++) {
+            const newDot = document.createElement("hr");
+            const dot_top = "top-[" + (Math.floor(Math.random() * 20) + 220) + "px]";
+            const ani_delay = Math.random() * 6 + "s";
+            newDot.classList.add("dot", dot_top, 'z-10');
+            newDot.style.animationDelay = ani_delay;
+            const dots = document.getElementById("dots");
+            dots === null || dots === void 0 ? void 0 : dots.appendChild(newDot);
+        }
+    }
+    gameOver() {
+    }
+    restartGame() {
+    }
+    score() {
     }
 }
 const dino = new Dino();
-dino.speed = 20;
-console.log(dino.speed);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 class Dino {
   private _jump_mt: number = 1;
   private _speed_num: number = 1;
-
+  readonly header_text_one: string = "T-Rex Chrome Dino Game";
+  readonly text_one: string =
+    "T-Rex Dinosaur - a replica of the hidden game from Chrome offline mode. Press Space to start the game online and jump your Dino, use down arrow (↓) to duck.";
+  private readonly _t_rex = document.getElementById(
+    "t_rex"
+  ) as HTMLImageElement;
   constructor() {
     this._jump_mt = this._jump_mt;
     this._speed_num = this._speed_num;
+    this.game_logic_control();
   }
   get speed(): number {
     return this._speed_num;
@@ -18,24 +24,39 @@ class Dino {
   set jump_mt(num: number) {
     if (num > 0) this._jump_mt = num;
   }
+  game_logic_control(){
+    this.ground_dots_moving();
+  }
   jump() {
     //jump by press space key and _jump_mt
   }
   trees_moving() {
     //handle trees moving with ._speed_num
-    //handle trees random size 
+    //handle trees random size
     //handle cloudes moving with ._speed_num
     //increase speed every minute
+  }
+  ground_dots_moving() {
+    for (let i = 1; i < 500; i++) {
+      const newDot: HTMLHRElement = <HTMLHRElement>document.createElement("hr");
+      const dot_top: string = "top-[" + (Math.floor(Math.random() * 20) + 220) + "px]";
+      const ani_delay: string = Math.random() * 6 + "s";
+      newDot.classList.add("dot", dot_top, 'z-10');
+      newDot.style.animationDelay = ani_delay;
+      const dots = document.getElementById("dots");
+      dots?.appendChild(newDot);
+    }    
   }
   gameOver() {
     //check game over!
   }
-  restartGame(){
+  restartGame() {
     //restart whole game
   }
-  score(){
+  score() {
     //claculate scores
     //show scores on board
   }
 }
 const dino = new Dino();
+
